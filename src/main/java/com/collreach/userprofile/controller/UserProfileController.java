6f374a0b758ba5a3957bfc44fc311524f6593cdb
ayush <ayushhurdey999@gmail.com>
2020-10-23 19:08:58 +0530

    Initial commit

---
+++ src/main/java/com/collreach/userprofile/controller/UserProfileController.java
@@ -0,0 +1,14 @@
+package com.collreach.userprofile.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/userprofile")
+public class UserProfileController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello...";
+    }
+}
---
+++ src/test/java/com/collreach/userprofile/UserProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collreach.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
