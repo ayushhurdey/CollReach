548e211a66701ce91627501bf0876ca71b9ebc9b
ayush <ayushhurdey999@gmail.com>
2020-10-24 12:21:06 +0530

    UserLoginRequest.java setters added.

--- src/main/java/com/collreach/userprofile/model/request/UserLoginRequest.java
+++ src/main/java/com/collreach/userprofile/model/request/UserLoginRequest.java
@@ -4,11 +4,20 @@ public class UserLoginRequest {
     private String userName;
     private String password;
 
+
     public String getUserName() {
         return userName;
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
