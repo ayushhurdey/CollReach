aa45278e497896af9c8b7679e0910d3ac6540580
ayush <ayushhurdey999@gmail.com>
2020-10-24 12:44:04 +0530

    API addNewUser() created.

--- src/main/java/com/collreach/userprofile/controller/UserProfileController.java
+++ src/main/java/com/collreach/userprofile/controller/UserProfileController.java
@@ -1,14 +1,33 @@
 package com.collreach.userprofile.controller;
 
+import com.collreach.userprofile.model.bo.User;
+import com.collreach.userprofile.model.repositories.UserRepository;
+import com.collreach.userprofile.model.request.UserAddRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
-@RequestMapping("/userprofile")
+@Controller
+@RequestMapping(path="/user")
 public class UserProfileController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/")
+    public ResponseEntity<String> addNewUser(@RequestBody UserAddRequest userAddRequest){
+        User user = new User();
+        user.setUserName(userAddRequest.getUserName());
+        user.setPassword(userAddRequest.getPassword());
+        userRepository.save(user);
+        return ResponseEntity.ok().body("User Added Successfully");
+    }
+
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
+
         return "Hello...";
     }
 
