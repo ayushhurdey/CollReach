19990352b95289a763fbc318d16e78b3840e86d2
ayush <ayushhurdey999@gmail.com>
2020-10-24 15:41:20 +0530

    made request mapping corrections in request

--- src/main/java/com/collreach/userprofile/model/request/UserLoginRequest.java
+++ src/main/java/com/collreach/userprofile/model/request/UserLoginRequest.java
@@ -2,30 +2,18 @@ package com.collreach.userprofile.model.request;
 
 public class UserLoginRequest {
     int id;
-    private String username;
+    private String userName;
     private String password;
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public String getUserName() {
-        return username;
-    }
+    public String getUserName() { return userName; }
 
-    public void setUserName(String userName) {
-        this.username = userName;
-    }
+    public void setUserName(String userName) { this.userName = userName; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) { this.password = password; }
 }
