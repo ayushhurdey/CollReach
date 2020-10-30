2316f6c1688742ccb844fc3b357063a239f31983
ayush <ayushhurdey999@gmail.com>
2020-10-31 00:12:43 +0530

    Added new attributes to signup schema

--- src/main/java/com/collreach/userprofile/model/bo/User.java
+++ src/main/java/com/collreach/userprofile/model/bo/User.java
@@ -14,6 +14,21 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private int yearOfStudy;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String branch;
+
+    @Column(nullable = false)
+    private String course;
+
     public int getId() { return id; }
 
     public void setId(int id) {
@@ -35,4 +50,24 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public int getYearOfStudy() { return yearOfStudy; }
+
+    public void setYearOfStudy(int yearOfStudy) { this.yearOfStudy = yearOfStudy; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getBranch() { return branch; }
+
+    public void setBranch(String branch) { this.branch = branch; }
+
+    public String getCourse() { return course; }
+
+    public void setCourse(String course) { this.course = course; }
 }
