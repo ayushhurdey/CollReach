cf38b7eb0c27b04e7bf42d60f967e7fa7da6c30a
ayush <ayushhurdey999@gmail.com>
2020-10-31 00:18:02 +0530

    added the modified attributes to userAddRequest.java

--- src/main/java/com/collreach/userprofile/model/request/UserAddRequest.java
+++ src/main/java/com/collreach/userprofile/model/request/UserAddRequest.java
@@ -4,6 +4,11 @@ public class UserAddRequest {
     private int id;
     private String userName;
     private String password;
+    private String email;
+    private String name;
+    private int yearOfStudy;
+    private String branch;
+    private String course;
 
     public int getId() {
         return id;
@@ -28,4 +33,24 @@ public class UserAddRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getYearOfStudy() { return yearOfStudy; }
+
+    public void setYearOfStudy(int yearOfStudy) { this.yearOfStudy = yearOfStudy; }
+
+    public String getBranch() { return branch; }
+
+    public void setBranch(String branch) { this.branch = branch; }
+
+    public String getCourse() { return course; }
+
+    public void setCourse(String course) { this.course = course; }
 }
